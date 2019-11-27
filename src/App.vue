e3cb00980b17b75ef3dcd6aaee1ff3498fa9c729
shindousaigo <597917872@qq.com>
2019-11-27 17:54:05 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+    <Prompt></Prompt>
+    <LoadingBar></LoadingBar>
+    <Loading></Loading>
+  </div>
 </template>
-<script>
 
+<script>
+import './assets/scss/index.scss'
+import Prompt from './components/prompt/prompt.vue'
+import LoadingBar from './components/loadingbar/loadingbar.vue'
+import Loading from './components/loading/loading.vue'
+export default {
+  name: 'app',
+  components: {
+    Prompt,LoadingBar,Loading
+  }
+}
 </script>
+
 <style>
-    @import "./assets/css/main.css";
-    @import "./assets/css/color-dark.css";     /*深色主题*/
-    /*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
+html {
+  position: relative;
+}
+
+html,
+body,
+#app {
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,80 +1,41 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from 'vue'
+import Router from 'vue-router'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-    routes: [{
-            path: '/',
-            redirect: '/userManager'
-        },
-        {
-            path: '/',
-            component: () =>
-                import ( /* webpackChunkName: "home" */ '../components/common/Home.vue'),
-            meta: { title: '自述文件' },
-            children: [{
-                    path: '/user-*',
-                },
-                {
-                    path: '/userManager',
-                    component: () =>
-                        import ('../components/page/userManager/index.vue'),
-                    meta: { title: '用户管理' }
-                },
-                {
-                    path: '/roleList',
-                    component: () =>
-                        import ('../components/page/roleList/index.vue'),
-                    meta: { title: '角色列表' }
-                },
-                {
-                    path: '/systemManager',
-                    component: () =>
-                        import ('../components/page/systemManager/index.vue'),
-                    meta: { title: '系统管理' }
-                },
-                {
-                    path: '/menusManager',
-                    component: () =>
-                        import ('../components/page/menusManager/index.vue'),
-                    meta: { title: '菜单管理' }
-                },
-                {
-                    path: '/zoneManager',
-                    component: () =>
-                        import ('../components/page/zoneManager/index.vue'),
-                    meta: { title: '区服管理' }
-                },
-                {
-                    path: '/gameList',
-                    component: () =>
-                        import ('../components/page/gameList/index.vue'),
-                    meta: { title: '游戏列表' }
-                },
-                {
-                    path: '/channelManager',
-                    component: () =>
-                        import ('../components/page/channelManager/index.vue'),
-                    meta: { title: '渠道管理' }
-                },
-                {
-                    path: '/404',
-                    component: () =>
-                        import ('../components/page/404.vue'),
-                    meta: { title: '404' }
-                },
-            ]
-        },
-        {
-            path: '/login',
-            component: () =>
-                import ('../components/page/Login.vue'),
-            meta: { title: '登录' }
-        },
-        {
-            path: '*',
-            redirect: '/404'
+  mode: 'hash', // Demo is living in GitHub.io, so required!
+  linkActiveClass: 'open active',
+  routes: [{
+      path: '/',
+      name: 'index',
+      redirect:'login',
+      component: (resolve) => {
+        require(['@/views/index.vue'], resolve)
+      },
+      children: [{
+        path: 'role-manage',
+        name: 'role-manage',
+        component: (resolve) => {
+          require(['@/views/modules/role-manage/index.vue'], resolve)
         }
-    ]
-});
+      },{
+        path: 'user-manage',
+        name: 'user-manage',
+        component: (resolve) => {
+          require(['@/views/modules/user-manage/index.vue'], resolve)
+        }
+      }]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: (resolve) => {
+        require(['@/views/login.vue'], resolve)
+      }
+    },{
+      path: '*',
+      redirect: 'login',
+    }
+  ]
+})
